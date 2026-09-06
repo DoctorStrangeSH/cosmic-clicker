60d0417b5a655ff375a7f9269e4d84e7dd82c6a9
Vladislav Shteinardt <sherlockds@yandex-team.ru>
2026-09-06 14:09:53 +0300

    Fix: relative paths for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/cosmic-clicker/', // Для GitHub Pages с username DoctorStrangeSH
+  base: './', // Важно! Используем относительные пути
   server: {
     port: 3000,
     host: true
